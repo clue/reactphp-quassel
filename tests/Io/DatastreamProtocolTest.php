c1da34cef21c28f69f0ba3ea23291582c37279e7
Christian Lück <christian@lueck.tv>
2015-08-20 22:34:35 +0200

    New datastream protocol should be strictly compatible to legacy one

--- tests/Io/DatastreamProtocolTest.php
+++ tests/Io/DatastreamProtocolTest.php
@@ -13,4 +13,23 @@ class DatastreamProtocolTest extends AbstractProtocolTest
     {
         $this->assertFalse($this->protocol->isLegacy());
     }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testCanNotTransportListStartingWithString()
+    {
+        $this->protocol->writeVariantList(array('does', 'not', 'work'));
+    }
+
+    public function testInitDataWireFormatWillBeRepresentedLikeLegacyProtocol()
+    {
+        // the message as it is sent over the wire
+        $message = array(Protocol::REQUEST_INITDATA, 'Network', '1', 'k1', 'v1', 'k2', 'v2');
+
+        // the actual message interpretation (in line with legacy protocol wire format)
+        $expected = array(Protocol::REQUEST_INITDATA, 'Network', '1', array('k1' => 'v1', 'k2' => 'v2'));
+
+        $this->assertEquals($expected, $this->protocol->readVariant($this->protocol->writeVariantList($message)));
+    }
 }
